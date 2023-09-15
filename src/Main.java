f2a903dca25ffcf6aed59627d1db8e21584a4516
ganadipa <13522055@mahasiswa.itb.ac.id>
2023-09-16 00:36:50 +0700

    Done get input n read matrix from console

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Matrix m1 = new Matrix(5, 5);
+        m1.readMatrix();
+        m1.displayMatrix(null);
+    }
+}
---
+++ src/Utils/Utils.java
@@ -0,0 +1,35 @@
+package Utils;
+
+import java.util.*;
+
+public class Utils {
+
+
+    private static Scanner userInput = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+    }
+
+    // Jika gagal mendapatkan input dari user, mengembalikan -1.
+    public static int getIntInput(String message, String mismatchMessage)
+    {
+
+        int res = -1;
+        System.out.print(message);
+        while (res == -1) {
+            try {
+                res = userInput.nextInt();
+                if (res == -1) break;
+            } catch (InputMismatchException e) {
+                System.out.println(mismatchMessage);
+            } 
+        }
+
+        return res;
+    }
+
+    
+
+    // 
+
+}
